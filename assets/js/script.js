0f388af4c558368016d644f13ec4d0332865146e
Gaurav Jagpal <gauravjagpal@hotmail.com>
2024-03-10 20:05:07 +0000

    add js functions

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementIncorrectScore() {
+
+}
+
+function incrementTotalQuestions() {
+    
+}
